42b5c7a9583e21cafec85d1a89a9674a323a16dd
Manish Shahi <manish.shahi@mnsu.edu>
2018-03-28 17:56:41 +0000

    Changed functions and some other stuff for testing

--- rubygame/app/controllers/landing_controller.rb
+++ rubygame/app/controllers/landing_controller.rb
@@ -5,7 +5,15 @@ class LandingController < ApplicationController
     @client = RubyGg::Client.new("RGAPI-89538f52-2141-443c-aaaa-9cb0d44ea380", 'na') #I'll PM you guys the api_key, don't push it to github. 
     challenger = @client.challenger.solo_queue(5)
     @challengerinfo = []
-    challenger.each {|x| @challengerinfo.push(@client.summoner.find(x[:playerOrTeamName]))}
+    # challenger.each {|x| @challengerinfo.push(@client.summoner.find(x[:playerOrTeamName]))}
+    @challengerinfo.push(@client.summoner.find('rockerturner'))
+    # @challengerinfo.push(@client.summoner.find('yb50110'))
+    @challengerinfo.push(@client.summoner.find('Knuckleś'))
+    @challengerinfo.push(@client.summoner.find('catdg'))
+    @challengerinfo.push(@client.summoner.find('zedberg'))
+    @challengerinfo.push(@client.summoner.find('owlfile'))
   end
+  
+  
   
 end
