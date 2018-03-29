082d6e286bdc5eca00523935972d72abddc764fb
Brhane Gebregziabher <brhane.gebregziabher@mnsu.edu>
2018-03-29 01:51:37 +0000

    commit

--- rubygame/app/controllers/auth_controller.rb
+++ rubygame/app/controllers/auth_controller.rb
@@ -1,7 +1,27 @@
 class AuthController < ApplicationController
-  def index
-  end
+  #def index
+  #end
 
-  def create
-  end
+  #def create
+  #end
+  
+  #below this line is added
+  skip_before_action:authorize
+    
+    def new
+    end
+    def create
+        user=User.find_by(name:params[:user_email])
+    if user and user.authenticate(params[:user_password])
+        sessions[:user_id]=user.id
+        redirect_to admin_url
+    else
+        redirect_to login_url, alert:"invaldi username or"
+        
+    end
+    end
+    def destroy
+        sessions[:user_id]=nil
+        redirect_to login_url, alert:"success"
+    end
 end
