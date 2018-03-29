4638a24005994eafe12cd436adf2231a9de0babd
Brhane Gebregziabher <brhane.gebregziabher@mnsu.edu>
2018-03-29 23:12:24 +0000

    user loging done at conrtoller auth_controller

--- rubygame/app/controllers/auth_controller.rb
+++ rubygame/app/controllers/auth_controller.rb
@@ -12,13 +12,13 @@ class AuthController < ApplicationController
     end
     def create
         user=User.find_by(name:params[:user_email])
-    if user and user.authenticate(params[:user_password])
-        sessions[:user_id]=user.id
-        redirect_to auth_url
-    else
-        redirect_to login_url, alert:"invaldi username or"
+        if user and user.authenticate(params[:user_password])
+            sessions[:user_id]=user.id
+            redirect_to auth_url
+        else
+           redirect_to login_url, alert:"invaldi username or"
         
-    end
+        end
     end
     def destroy
         sessions[:user_id]=nil
