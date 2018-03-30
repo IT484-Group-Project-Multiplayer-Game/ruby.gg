b7fc1388918748eb9d819979b642105ebe15a964
Rashad Khan <rashad.khan@mnsu.edu>
2018-03-30 20:28:00 +0000

    Added Google sign in button image

--- rubygame/app/controllers/users_controller.rb
+++ rubygame/app/controllers/users_controller.rb
@@ -21,8 +21,16 @@ class UsersController < ApplicationController
     end
     
     helper_method :current_user
-
+    
+    #skip_before_filter :require_login
     def current_user
         @current_user ||= User.find(session[:user_id]) if session[:user_id]
+        rescue ActiveRecord::RecordNotFound
     end
+    
+    #before_filter :require_user
+
+    #def require_user
+        #redirect_to users_path unless current_user
+    #end
 end
