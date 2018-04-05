2759671136307adeed2fb7ea43362f27a4a59b1a
Rashad Khan <rashad.khan@mnsu.edu>
2018-04-05 22:18:22 +0000

    Updates

---
+++ rubygame/app/controllers/summoner_controller.rb
@@ -0,0 +1,26 @@
+require "ruby_gg"
+class SummonerController < ApplicationController
+ 
+  def index
+    @client = RubyGg::Client.new("RGAPI-89538f52-2141-443c-aaaa-9cb0d44ea380", 'na') #I'll PM you guys the api_key, don't push it to github. 
+    challenger = @client.challenger.solo_queue(6)
+    @challengerinfo = []
+    challenger.each {|x| @challengerinfo.push(@client.summoner.find(x[:playerOrTeamName]))}
+    # @challengerinfo.push(@client.summoner.find('rockerturner'))
+    # # @challengerinfo.push(@client.summoner.find('yb50110'))
+    # @challengerinfo.push(@client.summoner.find('Knuckleś'))
+    # @challengerinfo.push(@client.summoner.find('catdg'))
+    # @challengerinfo.push(@client.summoner.find('zedberg'))
+    # @challengerinfo.push(@client.summoner.find('owlfile'))
+  end
+  
+  def search
+    
+  end
+  
+
+  def show
+
+  end
+
+end
