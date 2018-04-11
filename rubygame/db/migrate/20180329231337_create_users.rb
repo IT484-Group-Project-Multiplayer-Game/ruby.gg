c15dbbd177c701a5936e68d2f0ca5122dc287200
Brhane Gebregziabher <brhane.gebregziabher@mnsu.edu>
2018-04-11 08:24:26 +0000

    database corrected

--- rubygame/db/migrate/20180329231337_create_users.rb
+++ rubygame/db/migrate/20180329231337_create_users.rb
@@ -1,10 +1,6 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :user_email
-      t.string :password_hash
-      t.string :password_salt
-      
       t.string :provider
       t.string :uid
       t.string :name
