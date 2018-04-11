a34875a80a43bc0d5925b2a9fb8ae77f738dcc23
Brhane Gebregziabher <brhane.gebregziabher@mnsu.edu>
2018-04-11 08:40:53 +0000

    commit

--- rubygame/app/models/user.rb
+++ rubygame/app/models/user.rb
@@ -1,27 +1,4 @@
 class User < ActiveRecord::Base
-      attr_accessor :password
-  before_save :encrypt_password
-  
-  validates_confirmation_of :password
-  validates_presence_of :password, :on => :create
-  validates_presence_of :user_email
-  validates_uniqueness_of :user_email
-  
-  def self.authenticate(email, password)
-    user = find_by_email(email)
-    if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)
-      user
-    else
-      nil
-    end
-  end
-  
-  def encrypt_password
-    if password.present?
-      self.password_salt = BCrypt::Engine.generate_salt
-      self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
-    end
-  end
     def self.from_omniauth(auth)
         where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
           user.provider = auth.provider
