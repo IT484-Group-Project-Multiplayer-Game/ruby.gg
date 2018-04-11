af961409f49e19805438382275632a789750b0cd
Brhane Gebregziabher <brhane.gebregziabher@mnsu.edu>
2018-04-11 08:42:44 +0000

    commit

--- rubygame/app/controllers/users_controller.rb
+++ rubygame/app/controllers/users_controller.rb
@@ -2,17 +2,9 @@ class UsersController < ApplicationController
     def index
     end
     def new
-        @user = User.new
     end
 
     def create
-       params.permit!
-       @user = User.new(params[:user])
-        if @user.save
-            redirect_to root_url, :notice => "user created sucessfully"
-        else
-            render "new"
-        end
     end
 
 
