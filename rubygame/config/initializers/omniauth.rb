81f1f1467b73e90d4d578519f493976d147ef1a1
Rashad Khan <rashad.khan@mnsu.edu>
2018-04-11 16:32:06 +0000

    Gemfile and omniauth updated

--- rubygame/config/initializers/omniauth.rb
+++ rubygame/config/initializers/omniauth.rb
@@ -1,4 +1,5 @@
 # Replace API_KEY and API_SECRET with the values you got from Google
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, "747795763712-ai0g80onq9f8a3kin1nqp67fk1cbipp7.apps.googleusercontent.com", "3iLJlt7ZMqpZF7-BBb7745Ch"
+  provider :twitch, ENV["ylrk7gp4zgg3xofr558af6403x13yb"], ENV["hmzv19zjarfviux23sx4gk03gy86mu"]
 end
