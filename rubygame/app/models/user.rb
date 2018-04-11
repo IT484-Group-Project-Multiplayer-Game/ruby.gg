b2e3239254baf3e191a4c33cb0dc811ca11a2c5a
Brhane Gebregziabher <brhane.gebregziabher@mnsu.edu>
2018-04-11 08:36:29 +0000

    validation modified

--- rubygame/app/models/user.rb
+++ rubygame/app/models/user.rb
@@ -4,8 +4,8 @@ class User < ActiveRecord::Base
   
   validates_confirmation_of :password
   validates_presence_of :password, :on => :create
-  validates_presence_of :email
-  validates_uniqueness_of :email
+  validates_presence_of :user_email
+  validates_uniqueness_of :user_email
   
   def self.authenticate(email, password)
     user = find_by_email(email)
