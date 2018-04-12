b1c37daa4211e01ab0001fb9d99f07d37d63ad10
Manish Shahi <manish.shahi@mnsu.edu>
2018-04-12 23:35:35 +0000

    Api key saved in environment variable. I recommend you guys do the same

--- rubygame/app/controllers/summoner_controller.rb
+++ rubygame/app/controllers/summoner_controller.rb
@@ -2,7 +2,7 @@ require "ruby_gg"
 class SummonerController < ApplicationController
  
   def index
-    @client = RubyGg::Client.new("RGAPI-89538f52-2141-443c-aaaa-9cb0d44ea380", 'na') #I'll PM you guys the api_key, don't push it to github. 
+    @client = RubyGg::Client.new(ENV['league_api_key'], 'na') #I'll PM you guys the api_key, don't push it to github. 
     challenger = @client.challenger.solo_queue(6)
     @challengerinfo = []
     challenger.each {|x| @challengerinfo.push(@client.summoner.find(x[:playerOrTeamName]))}
