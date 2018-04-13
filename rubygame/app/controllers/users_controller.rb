1d5dcacfca814c5e096a6b4c0ef3baeed02affe2
Brhane Gebregziabher <brhane.gebregziabher@mnsu.edu>
2018-04-13 15:09:56 +0000

    controller to connect both view and database

--- rubygame/app/controllers/users_controller.rb
+++ rubygame/app/controllers/users_controller.rb
@@ -1,79 +1,51 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
+    def index
+    end
+    def new
+        @user = User.new
+    end
 
-  # GET /users
-  # GET /users.json
-  def index
-    @users = User.all
-  end
-
-  # GET /users/1
-  # GET /users/1.json
-  def show
-  end
-
-  # GET /users/new
-  def new
-    @user = User.new
-  end
-
-  # GET /users/1/edit
-  def edit
-  end
-
-  # POST /users
-  # POST /users.json
-  
+    def create
+     params.permit!
+    # @user = User.new(params[:user])
+     @user = User.new(user_params)
+        if @user.save
+        redirect_to root_url, :notice => "user created successfully!"
+        else
+           render "new"
+            
+        end
+    end
     
-  def create
-   @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to landing_index_path, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-        #redirect_to landing_index_path
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
-
-  end
-
-
-  # PATCH/PUT /users/1
-  # PATCH/PUT /users/1.json
-  def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to users_url, notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /users/1
-  # DELETE /users/1.json
-  def destroy
-    @user.destroy
-    respond_to do |format|
-      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
+    
+    private
     def user_params
-      params.permit(:user_email, :user_password, :password_confirmation)
+      #params.require(:user).permit(:email, :password, :password_confirmation)
+      params.permit(:email, :password, :password_confirmation)
     end
+
+    def show
+        @user ||= User.find(session[:user_id]) if session[:user_id]
+        rescue ActiveRecord::RecordNotFound
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+    
+    helper_method :current_user
+    
+    #skip_before_filter :require_login
+    def current_user
+        @current_user ||= User.find(session[:user_id]) if session[:user_id]
+        rescue ActiveRecord::RecordNotFound
+    end
+    
+    #before_filter :require_user
+
+    #def require_user
+        #redirect_to users_path unless current_user
+    #end
 end
