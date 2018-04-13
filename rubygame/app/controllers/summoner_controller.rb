a51bb4fbbe70826969a325ef107b561cc6068dff
Manish Shahi <manish.shahi@mnsu.edu>
2018-04-13 00:37:54 +0000

    Made the client global by defining it as class variable in application controller.rb and spewed data into show

--- rubygame/app/controllers/summoner_controller.rb
+++ rubygame/app/controllers/summoner_controller.rb
@@ -2,16 +2,11 @@ require "ruby_gg"
 class SummonerController < ApplicationController
  
   def index
-    @client = RubyGg::Client.new(ENV['league_api_key'], 'na') #I'll PM you guys the api_key, don't push it to github. 
-    challenger = @client.challenger.solo_queue(6)
+    @client = @@client #I'll PM you guys the api_key, don't push it to github. 
+    challenger = @client.challenger.solo_queue(5)
     @challengerinfo = []
     challenger.each {|x| @challengerinfo.push(@client.summoner.find(x[:playerOrTeamName]))}
-    # @challengerinfo.push(@client.summoner.find('rockerturner'))
-    # # @challengerinfo.push(@client.summoner.find('yb50110'))
-    # @challengerinfo.push(@client.summoner.find('Knuckleś'))
-    # @challengerinfo.push(@client.summoner.find('catdg'))
-    # @challengerinfo.push(@client.summoner.find('zedberg'))
-    # @challengerinfo.push(@client.summoner.find('owlfile'))
+    
   end
 
   def search
@@ -25,7 +20,8 @@ class SummonerController < ApplicationController
   end
 
   def show
-
+    @client = @@client
+    @summonerIGN = params[:ign]
   end
 
 end
