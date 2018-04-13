da01ed240f9813bfa2a06f682d95e959b9605332
Brhane Gebregziabher <brhane.gebregziabher@mnsu.edu>
2018-04-13 03:21:36 +0000

    database,signup and signin complete-by brhane

--- rubygame/config/routes.rb
+++ rubygame/config/routes.rb
@@ -17,10 +17,7 @@ Rails.application.routes.draw do
 
   resources :sessions, only: [:create, :destroy]
   resource :home, only: [:show]
-  
-  
-  
-
+  #ROUTES 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
