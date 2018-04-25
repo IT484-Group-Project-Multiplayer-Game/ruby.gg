09b26011069364f0458eca9245402eedde3ed44b
Manish Shahi <manish.shahi@mnsu.edu>
2018-04-25 03:49:31 +0000

    initial changes

--- rubygame/app/controllers/summoner_controller.rb
+++ rubygame/app/controllers/summoner_controller.rb
@@ -1,11 +1,10 @@
-require "ruby_gg"
+
 class SummonerController < ApplicationController
  
   def index
-    @client = RubyGg::Client.new("RGAPI-89538f52-2141-443c-aaaa-9cb0d44ea380", 'na') #I'll PM you guys the api_key, don't push it to github. 
-    challenger = @client.challenger.solo_queue(5)
+    challenger = @@client.challenger.solo_queue(5)
     @challengerinfo = []
-    challenger.each {|x| @challengerinfo.push(@client.summoner.find(x[:playerOrTeamName]))}
+    challenger.each {|x| @challengerinfo.push(@@client.summoner.find(x[:playerOrTeamName]))}
   end
 
   def search
@@ -28,11 +27,10 @@ class SummonerController < ApplicationController
   end
 
   def favoritesIndex
-    @client = RubyGg::Client.new("RGAPI-89538f52-2141-443c-aaaa-9cb0d44ea380", 'na')
     favoriteSummoners = Favorite.where(:user => current_user.id).pluck(:summoner)
     @summonerInfo = []
     favoriteSummoners.each { |x|
-        @summonerInfo.push(@client.summoner.find(x))
+        @summonerInfo.push(@@client.summoner.find(x))
         }
   end
 
@@ -64,7 +62,7 @@ class SummonerController < ApplicationController
 
         flash[:notice] = "Successfully removed summoner #{summoner} from Favorites"
         redirect_to summoner_show_path(:ign => summoner)
-    end
+     end
   end
 
 end
