eac5de06f6755d54627a299616da04386825666b
Manish Shahi <manish.shahi@mnsu.edu>
2018-04-27 00:58:08 +0000

    new changes and cache

--- rubygame/db/schema.rb
+++ rubygame/db/schema.rb
@@ -20,14 +20,4 @@ ActiveRecord::Schema.define(version: 20180420011350) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string   "provider"
-    t.string   "uid"
-    t.string   "name"
-    t.string   "oauth_token"
-    t.datetime "oauth_expires_at"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
-  end
-
 end
