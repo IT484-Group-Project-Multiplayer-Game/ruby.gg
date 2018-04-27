d4660a9672103c8983f4fba470bc801cc670a81e
yunhaseo <yunha@b302.nl>
2018-04-26 20:26:21 -0500

    some stuff for favorite and schema updates

--- rubygame/db/schema.rb
+++ rubygame/db/schema.rb
@@ -20,4 +20,17 @@ ActiveRecord::Schema.define(version: 20180420011350) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "password_hash"
+    t.string   "password_salt"
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "name"
+    t.string   "oauth_token"
+    t.datetime "oauth_expires_at"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+  end
+
 end
